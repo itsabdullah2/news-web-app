f02f858ca5cf4fb337a2bc78fa07ec9179a7bf0d
itsabdullah2 <abdullahosman3020@gmail.com>
2025-02-04 21:16:43 +0200

    handle errors and check if the response is ok

--- src/services/index.ts
+++ src/services/index.ts
@@ -16,7 +16,16 @@ export const getData = async (category: string, setArticles: SetArticles) => {
   const API_URL = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
 
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch(API_URL, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
     const data = await response.json();
 
     setArticles(data.articles);
